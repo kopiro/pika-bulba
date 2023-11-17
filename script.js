f31cf678f27f5e5aa7293ede74a16fc3957df1c9
Flavio De Stefano <destefano.flavio@gmail.com>
2023-11-17 22:32:22 +0100

    Applying x noise

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const IMG_OFFSET = 30;
 const FPS = 1000 / 60;
 const MAX_ADVANCE = 100;
 const ROAD_METERS = 1000;
+const X_NOISE = 2;
 
 const GOAL = MAX_ADVANCE * 300;
 
@@ -27,7 +28,12 @@ const $xOffsets = {
   bulbasuck: IMG_OFFSET,
 };
 
-const $position = {
+const zPosition = {
+  pikacute: 0,
+  bulbasuck: 0,
+};
+
+const xPosition = {
   pikacute: 0,
   bulbasuck: 0,
 };
@@ -45,31 +51,36 @@ const $progress = {
 let winner = null;
 
 function positionImage(selector) {
-  const ratio = Math.min(1, $position[selector] / GOAL);
+  const ratio = Math.min(1, zPosition[selector] / GOAL);
   const z = Z_START + (Z_END - Z_START) * ratio;
 
   $images[selector].src = `./${selector}/${1 + $frames[selector]}.png`;
-  $images[
-    selector
-  ].style.transform = `translate3d(${$xOffsets[selector]}px, ${CUBE_SIZE}px, ${z}px)`;
+  $images[selector].style.transform = `translate3d(${
+    xPosition[selector] + $xOffsets[selector]
+  }px, ${CUBE_SIZE}px, ${z}px)`;
 
-  $progress[selector].textContent = `${Math.floor(ROAD_METERS * ratio)}m`;
+  const meters = Math.floor(ROAD_METERS * ratio);
+  $progress[selector].textContent = `${meters}m`;
   $progress[selector].style.transform = `translateX(-${(1 - ratio) * 100}%)`;
 }
 
 function makeItRun(selector) {
   requestAnimationFrame(() => {
     const advanceBy = Math.floor(Math.random() * MAX_ADVANCE);
-    $position[selector] = Math.min(GOAL, $position[selector] + advanceBy);
+
+    zPosition[selector] = Math.min(GOAL, zPosition[selector] + advanceBy);
     $frames[selector] = ($frames[selector] + 1) % $maxFrames[selector];
+
+    // Apply a bit of noise to the x position
+    xPosition[selector] += -X_NOISE + Math.random() * X_NOISE * 2;
 
     positionImage(selector);
 
-    if ($position[selector] === GOAL) {
-      $images[selector].src = `./${selector}/win.gif`;
-
+    if (zPosition[selector] === GOAL) {
       if (!winner) {
         declareWinner(selector);
+      } else {
+        declareLoser(selector);
       }
       return;
     }
@@ -82,8 +93,13 @@ function makeItRun(selector) {
 
 function declareWinner(selector) {
   winner = selector;
+  $images[selector].src = `./${selector}/win.gif`;
   $winner.textContent = `${selector} won!`;
   $winner.classList.add(`${selector}-color`);
+}
+
+function declareLoser(selector) {
+  $images[selector].src = `./${selector}/lost.gif`;
 }
 
 function preloadImages(selector) {
