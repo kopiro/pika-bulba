1a5112c53598f7e55784cdf4d7a338ab55cbe9cd
Flavio De Stefano <destefano.flavio@gmail.com>
2023-11-26 18:45:58 +0100

    Giving boost

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const playerKeys = Object.keys(config.players);
 const $ = document.querySelector.bind(document);
 
-const $css = getComputedStyle(document.documentElement);
-
+// DOM elements
 const $winner = $("#winner");
 const $startAutorace = $("#start-autorace");
 const $startCoop = $("#start-coop");
@@ -12,82 +11,126 @@ const $resetButton = $("#reset-button");
 const $start = $("#start");
 const $overlay = $("#overlay");
 
-const game = {
-  $: { rocks: [], bushes: [], players: {}, progress: {} },
-};
-
-// Static values from CSS
+// Static values from CSS so we can keep them in sync
+const $css = getComputedStyle(document.documentElement);
 const kTrackWidth = getCSSVar("--track-width");
 const kImgSize = getCSSVar("--img-size");
 const kInfiniteTrackLength = getCSSVar("--infinite-track-length");
 const kBushSize = getCSSVar("--bush-size");
 
-// Static values
+// Main game object
+const game = {
+  $: { rocks: [], bushes: [], players: {}, progress: {} },
+};
+
+// Constants
+
+// Fixed FPS to prevent the game from running too fast
 const kFPS = 60;
+// Frames per second we want to render
 const framesPerSecond = 1000 / kFPS;
-const kAutoMinAdvance = 1;
-const kAutoMaxAdvance = 10;
+// PX per frame we want to advance when game is running in auto mode, minimum and maximum
+const kAutoMinAdvance = 0.5;
+const kAutoMaxAdvance = 8;
+// Overshoot for the player to move along the X axis when game is running in auto mode
 const kXNoiseMax = 100;
+// PX per frame to apply to the player is falling
 const kGravity = (30 * 9.81) / kFPS;
 
-const kCoopMul = 1;
-const kCoopBoost = kCoopMul * kAutoMaxAdvance;
-const kCoopDecay = kCoopMul * 0.05;
+// PX per frame we want to advance when game is running in coop mode
+const kCoopBoost = kAutoMaxAdvance;
+// PX decay to remove every frame to slow down the player in coop mode
+const kCoopDecay = 0.05;
+// Maximum value for the boost even if key is pressed very fast
 const kMaxCoopBost = 3;
+// PX to move the player in the X axis when game is running in coop mode
 const kCoopX = 3;
 
+// PX to push the player in the X axis when colliding with another player
 const kPlayerCollisionPushX = 60;
-const kReachedPx = 0.5;
 
+// Epsilon to consider the player has reached the target X
+const kReachedPx = 0.1;
+
+// Offset to consider for the final flag
 const kGoalZOffset = 100;
+
+// Density of bushes and rocks
 const kRockDensity = 20;
 const kBushDensity = 30;
 
-const kCameraOffset = 600;
+// Camera distance from player
+const kCameraZOffset = 600;
+// Maximum value for the camera Z, to make sure we stop when goal is reached
 const kMaxCameraZ = -480;
+// Camera X and Y offsets
 const kCameraX = kTrackWidth / 4;
 const kCameraY = 400;
 
-function loadGame() {
-  readOptions();
+// If player is losing for this T, they will advance faster
+const kLosingTimeout = 2000;
 
+// Track width half
+const kTrackWidthHalf = kTrackWidth / 2;
+
+/**
+ * Read dynamic options and loads the game object
+ */
+function loadGame() {
   game.started = false;
   game.mode = null;
   game.winner = null;
   game.loser = null;
+  game.trackLength = $("#opt-track-length").value;
 
   game.$.players = playerKeys.reduce((acc, key) => {
+    const z = game.trackLength - (config.players[key].zOffset || 0);
     acc[key] = {
       ...acc[key],
       key: key,
       config: config.players[key],
       x: config.players[key].xOffset,
       y: 0,
-      z: game.trackLength,
+      z: z,
       frame: 0,
       coopValue: 0,
-      lastZ: game.trackLength,
-      playerPushX: null,
-      nextX: null,
+      next: null,
       jumpingT: null,
       wonT: null,
       fell: false,
     };
     return acc;
   }, game.$.players || {});
-
-  document.body.classList.remove("game-ended");
-  document.body.classList.remove("game-started");
-
-  $start.style.transform = `translate3d(-50%, -100%, ${
-    -1 * game.trackLength
-  }px)`;
 }
 
+/**
+ * Returns a random number between kAutoMinAdvance and kAutoMaxAdvance
+ * to be used as the advance for the player in auto mode
+ */
 function getAdvanceBy(p) {
-  return kAutoMinAdvance + Math.floor(Math.random() * kAutoMaxAdvance);
+  // Compare p to second player
+  const otherPlayer = playerKeys.find((key) => key !== p.key);
+  const diff = Math.abs(p.z - game.$.players[otherPlayer].z);
+
+  if (p.z > game.$.players[otherPlayer].z) {
+    p.losingSince = p.losingSince || now;
+  } else {
+    p.losingSince = null;
+  }
+
+  if (p.losingSince && now - p.losingSince > kLosingTimeout) {
+    return kAutoMaxAdvance;
+  }
+
+  return (
+    kAutoMinAdvance +
+    Math.floor(Math.random() * (kAutoMaxAdvance - kAutoMinAdvance))
+  );
 }
 
+/**
+ * Add the bushes to the scene
+ */
 function prepareSceneAddBushes() {
   [-kTrackWidth + kBushSize, kTrackWidth - kBushSize].forEach((_x) => {
     for (let i = 0; i < kBushDensity; i++) {
@@ -111,9 +154,12 @@ function prepareSceneAddBushes() {
   });
 }
 
+/**
+ * Add the rocks to the scene
+ */
 function prepareSceneAddRocks() {
   // Put the bushes in the scene
-  const maxX = kBushSize + kTrackWidth / 2 - kBushSize * 2;
+  const maxX = kTrackWidthHalf - kBushSize * 2;
   for (let i = 0; i < kRockDensity; i++) {
     const x = maxX - Math.random() * maxX * 2;
     const y = 0;
@@ -137,16 +183,23 @@ function prepareSceneAddRocks() {
   }
 }
 
+/**
+ * Quantize the coordinates of the player to the given tresholds
+ */
 function quantizeCoords({ x, y, z }, tresholds) {
   const c = {
     qx: Math.floor(x / tresholds.x),
-    qy: Math.floor(y, tresholds.y),
+    qy: Math.floor(y / tresholds.y),
     qz: Math.floor(z / tresholds.z),
   };
   return { ...c, key: `${c.qx}-${c.qy}-${c.qz}` };
 }
 
 const __rockCollisionCache = {};
+
+/**
+ * Check if the player is colliding with a rock
+ */
 function checkForRockCollision(p, tresholds) {
   const strThresholds = JSON.stringify(tresholds);
 
@@ -178,6 +231,10 @@ function checkForRockCollision(p, tresholds) {
   return collided;
 }
 
+/**
+ * Check if the player is colliding with another player
+ * and push them away from each other
+ */
 function checkForPlayerCollision() {
   // Detect collision between game.$.players
   const kImgSizeBody = kImgSize / 2.5;
@@ -195,13 +252,17 @@ function checkForPlayerCollision() {
       ) {
         // Push the players away from each other in the X axis
         const direction = p.x < p2.x ? -1 : 1;
-        p.playerPushX = {
+        p.next = {
           t: now,
           x: p.x + direction * kPlayerCollisionPushX,
+          easing: (t) => Math.pow(2, t),
+          reason: "player-collision",
         };
-        p2.playerPushX = {
+        p2.next = {
           t: now,
           x: p2.x + -1 * direction * kPlayerCollisionPushX,
+          easing: (t) => Math.pow(2, t),
+          reason: "player-collision",
         };
 
         // Show a quick red overlay
@@ -224,11 +285,8 @@ function checkForPlayerCollision() {
   }
 }
 
-async function prepareScene() {
-  prepareSceneAddBushes();
-  prepareSceneAddRocks();
-
-  Object.keys(game.$.players).forEach((key) => {
+function prepareSceneAddPlayers() {
+  playerKeys.forEach((key) => {
     const p = game.$.players[key];
 
     p.$ = (() => {
@@ -251,17 +309,33 @@ async function prepareScene() {
   });
 }
 
+/**
+ * Prepare the scene and add game objects to it
+ * This should be called only once
+ */
+async function prepareScene() {
+  prepareSceneAddBushes();
+  prepareSceneAddRocks();
+  prepareSceneAddPlayers();
+}
+
+/**
+ * Clamp the player coordinates to the track
+ */
 function clampPlayer(p) {
   // if (game.winner !== p) {
   //   p.x = Math.min(
-  //     kTrackWidth / 2 - kImgSize,
-  //     Math.max(-(kTrackWidth / 2) + kImgSize, p.x)
+  //     kTrackWidthHalf - kImgSize,
+  //     Math.max(-(kTrackWidthHalf) + kImgSize, p.x)
   //   );
   // }
   // p.y = Math.max(0, p.y);
   // p.z = Math.max(0, p.z);
 }
 
+/**
+ * Apply the coordinates to a game object by changing the CSS transform
+ */
 function applyCoordinates(p) {
   const cssX = p.x;
   const cssY = -p.y;
@@ -269,11 +343,21 @@ function applyCoordinates(p) {
   p.$.style.transform = `translate3d(${cssX}px, ${cssY}px, ${cssZ}px)`;
 }
 
-function moveCamera(z) {
-  const realZ = Math.max(z - kCameraOffset, kMaxCameraZ);
+/**
+ * Move the camera to the given Z
+ */
+function renderCamera() {
+  // Find the player with the lowest Z
+  const lowestZ = playerKeys.reduce((acc, key) => {
+    return Math.min(acc, game.$.players[key].z);
+  }, kInfiniteTrackLength);
+  const realZ = Math.max(lowestZ - kCameraZOffset, kMaxCameraZ);
   $scene.style.transform = `translate3d(${kCameraX}px, ${kCameraY}px, ${realZ}px)`;
 }
 
+/**
+ * Apply the given src to a game object
+ */
 function applySrc(obj, src) {
   if (obj.src !== src) {
     obj.src = src;
@@ -281,6 +365,9 @@ function applySrc(obj, src) {
   }
 }
 
+/**
+ * Render the player game object
+ */
 function renderPlayer(p) {
   clampPlayer(p);
   applyCoordinates(p);
@@ -294,7 +381,7 @@ function renderPlayer(p) {
   } else {
     applySrc(p, `./${p.key}/${p.frame + 1}.png`);
 
-    if (p.playerPushX) {
+    if (p.next && p.next.reason === "player-collision") {
       // Squeeze the player
       p.$.style.transform = `${p.$.style.transform} scaleX(0.85)`;
     }
@@ -305,7 +392,7 @@ function renderPlayer(p) {
   }
 
   if (p.fell) {
-    game.$.progress[p.key].firstChild.textContent = "out";
+    game.$.progress[p.key].firstChild.textContent = "out of track";
   } else {
     game.$.progress[p.key].firstChild.style.transform = `translateX(-${
       (1 - ratio) * 100
@@ -313,11 +400,20 @@ function renderPlayer(p) {
   }
 }
 
+function renderObjects() {
+  $start.style.transform = `translate3d(-50%, -100%, ${
+    -1 * game.trackLength
+  }px)`;
+}
+
 let then = null;
 let now = null;
 let deltaTime = null;
 let frameCount = 1;
 
+/**
+ * Main rendering loop capped to kFPS
+ */
 function startRenderingLoop() {
   requestAnimationFrame(startRenderingLoop);
 
@@ -328,24 +424,24 @@ function startRenderingLoop() {
   then = now - (deltaTime % framesPerSecond);
   frameCount++;
 
+  // Move objects
   if (game.started) {
-    Object.keys(game.$.players).forEach((key) => {
+    playerKeys.forEach((key) => {
       movePlayer(game.$.players[key]);
     });
   }
 
-  Object.keys(game.$.players).forEach((key) => {
+  // Render objects
+  renderObjects();
+  renderCamera();
+  playerKeys.forEach((key) => {
     renderPlayer(game.$.players[key]);
   });
-
-  // Find the player with the lowest Z
-  const lowestZ = Object.keys(game.$.players).reduce((acc, key) => {
-    return Math.min(acc, game.$.players[key].z);
-  }, kInfiniteTrackLength);
-
-  moveCamera(lowestZ);
 }
 
+/**
+ * Equation to move the player when they win
+ */
 function flyEquation(t) {
   return {
     x: -1 * t,
@@ -353,6 +449,9 @@ function flyEquation(t) {
   };
 }
 
+/**
+ * Equation to move the player when they jump
+ */
 function jumpEquation(t) {
   return {
     z: 0,
@@ -360,13 +459,49 @@ function jumpEquation(t) {
   };
 }
 
+/**
+ * Check if the player is out of the track and mark them as fell
+ */
 function checkForPlayerOutOfTrack(p) {
-  if (p.fell) return;
-  if (p.x < -kTrackWidth / 2 || p.x > kTrackWidth / 2) {
+  if (!p.fell && (p.x < -kTrackWidthHalf || p.x > kTrackWidthHalf)) {
     p.fell = true;
+    p.next = {
+      y: -999,
+      t: now,
+      reason: "fell",
+    };
   }
 }
 
+function maybeMovePlayerToNext(p) {
+  if (!p.next) return;
+
+  const delta = now - p.next.t;
+  let reached = true;
+
+  // Movement towards next based on time
+  ["x", "y", "z"].forEach((axis) => {
+    if (!p.next[axis]) return;
+
+    const diff = p.next[axis] - p[axis];
+    const step = diff / 10;
+    p[axis] =
+      p[axis] +
+      step * (p.next.easing ? p.next.easing(delta / 1000) : delta / 1000);
+
+    if (Math.abs(diff) >= kReachedPx) {
+      reached = false;
+    }
+  });
+
+  if (reached) {
+    p.next = null;
+  }
+}
+
+/**
+ * Move the player
+ */
 function movePlayer(p) {
   if (game.winner === p) {
     const delta = now - p.wonT;
@@ -382,31 +517,10 @@ function movePlayer(p) {
 
   checkForPlayerCollision();
   checkForPlayerOutOfTrack(p);
+  maybeMovePlayerToNext(p);
 
   if (p.fell) {
-    p.y = p.y - kGravity;
     return;
-  }
-
-  if (p.playerPushX) {
-    // Exponential movement towards playerPushX based on time
-    const diff = p.playerPushX.x - p.x;
-    const step = diff / 10;
-    const delta = now - p.playerPushX.t;
-    p.x = p.x + step * Math.pow(2, delta / 1000);
-    if (Math.abs(diff) <= kReachedPx) {
-      p.playerPushX = null;
-      p.nextX = null;
-    }
-  } else if (p.nextX) {
-    // Linear movement towards nextX based on time
-    const diff = p.nextX.x - p.x;
-    const step = diff / 10;
-    const delta = now - p.nextX.t;
-    p.x = p.x + (step * delta) / 1000;
-    if (Math.abs(diff) <= kReachedPx) {
-      p.nextX = null;
-    }
   }
 
   // Check if player is jumping
@@ -437,21 +551,21 @@ function movePlayer(p) {
       }
     }
 
-    // Add noise
-    if (!p.nextX) {
+    // Add noise if we don't already have a target for X
+    if (!p.next) {
       const xNoise = -kXNoiseMax + Math.random() * kXNoiseMax * 2;
-      const nextX = p.x + xNoise;
-      p.nextX = {
+      const next = p.x + xNoise;
+      p.next = {
         t: now,
         x: Math.min(
-          kTrackWidth / 2 - kImgSize,
-          Math.max(-kTrackWidth / 2 + kImgSize, nextX)
+          kTrackWidthHalf - kImgSize,
+          Math.max(-kTrackWidthHalf + kImgSize, next)
         ),
       };
     }
 
     // Advance Z
-    const advanceBy = getAdvanceBy(p.key);
+    const advanceBy = getAdvanceBy(p);
     p.z = Math.max(0, p.z - advanceBy);
   } else if (game.mode === "coop") {
     p.coopValue = Math.max(0, p.coopValue - kCoopDecay);
@@ -484,6 +598,9 @@ function movePlayer(p) {
   }
 }
 
+/**
+ * Declare the given player as the winner
+ */
 function declareWinner(p) {
   if (game.winner) return;
 
@@ -497,6 +614,9 @@ function declareWinner(p) {
   endGame();
 }
 
+/**
+ * Declare the given player as the loser
+ */
 function declareLoser(p) {
   if (game.loser) return;
 
@@ -504,10 +624,16 @@ function declareLoser(p) {
   game.$.progress[p.key].firstChild.textContent = "finished";
 }
 
+/**
+ * Returns the value of a CSS variable converted to a number
+ */
 function getCSSVar(key) {
   return Number($css.getPropertyValue(key).replace("px", ""));
 }
 
+/**
+ * Preload the images for the given player
+ */
 function preloadImages(key) {
   return new Promise((resolve) => {
     let loaded = 0;
@@ -536,35 +662,9 @@ function preloadImages(key) {
   });
 }
 
-function handleAccelerometer() {
-  // If we have an accelerometer in the device, use it to move X of the player based on the inclination of the phone (DeviceMotion)
-  if (
-    window.DeviceMotionEvent &&
-    typeof DeviceMotionEvent.requestPermission === "function"
-  ) {
-    // Request permissions
-    DeviceMotionEvent.requestPermission()
-      .then((permissionState) => {
-        if (permissionState === "granted") {
-          window.addEventListener("devicemotion", (event) => {
-            const x = event.accelerationIncludingGravity.x;
-            const y = event.accelerationIncludingGravity.y;
-            const z = event.accelerationIncludingGravity.z;
-
-            playerKeys.forEach((key) => {
-              game.$.players[key].x += x / 10;
-            });
-          });
-        }
-      })
-      .catch(console.error);
-  }
-}
-
-function readOptions() {
-  game.trackLength = $("#opt-track-length").value;
-}
-
+/**
+ * Start the game
+ */
 function startGame() {
   if (game.started) return;
   document.body.classList.add("game-started");
@@ -574,6 +674,9 @@ function startGame() {
   $("#music-bg").play();
 }
 
+/**
+ * End the game
+ */
 function endGame() {
   if (!game.started) return;
   document.body.classList.add("game-ended");
@@ -584,23 +687,23 @@ function endGame() {
   $("#music-end").play();
 }
 
+/**
+ * Preload the images for all players
+ */
 function preloadResources() {
-  return Promise.all(
-    Object.keys(game.$.players).map((key) => preloadImages(key))
-  );
+  return Promise.all(playerKeys.map((key) => preloadImages(key)));
 }
 
-async function main() {
+/**
+ * Main function, it will load the game and start the rendering loop
+ * and add event listeners to the DOM
+ */
+function main() {
   loadGame();
   prepareScene();
 
-  await preloadResources();
-
-  document.body.classList.add("game-ready");
-
   $startAutorace.addEventListener("click", () => {
     game.mode = "auto";
-    // handleAccelerometer();
     startGame();
   });
 
@@ -612,6 +715,8 @@ async function main() {
   $resetButton.addEventListener("click", () => {
     $("#music-end").pause();
     $("#music-bg").pause();
+    document.body.classList.remove("game-ended");
+    document.body.classList.remove("game-started");
     loadGame();
   });
 
@@ -621,34 +726,40 @@ async function main() {
 
   window.addEventListener("keyup", (e) => {
     e.preventDefault();
-    if (game.mode !== "coop") return;
+    e.stopPropagation();
 
-    Object.keys(game.$.players).forEach((key) => {
-      const p = game.$.players[key];
-      Object.entries(p.config.keystrokes).forEach(([keystroke, action]) => {
-        if (keystroke !== e.key) return;
-        switch (action) {
-          case "advance":
-            p.coopValue = Math.min(kMaxCoopBost, p.coopValue + kCoopBoost);
-            break;
-          case "jump":
-            p.jumpingT = p.jumpingT || now;
-            break;
-          case "left":
-            p.x -= kCoopX;
-            break;
-          case "right":
-            p.x += kCoopX;
-            break;
-          // case "back":
-          //   p.z += 10;
-          //   break;
-        }
+    if (game.mode === "coop") {
+      playerKeys.forEach((key) => {
+        const p = game.$.players[key];
+        Object.entries(p.config.keystrokes).forEach(([keystroke, action]) => {
+          if (keystroke !== e.key) return;
+          switch (action) {
+            case "advance":
+              p.coopValue = Math.min(kMaxCoopBost, p.coopValue + kCoopBoost);
+              break;
+            case "jump":
+              p.jumpingT = p.jumpingT || now;
+              break;
+            case "left":
+              p.x -= kCoopX;
+              break;
+            case "right":
+              p.x += kCoopX;
+              break;
+            // case "back":
+            //   p.z += 10;
+            //   break;
+          }
+        });
       });
-    });
+    }
   });
 
   startRenderingLoop();
+
+  preloadResources().then(() => {
+    document.body.classList.add("game-ready");
+  });
 }
 
 main();
